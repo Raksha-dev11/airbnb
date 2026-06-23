5a83f692b2c324f5aa2708a86de1a32cf66d23d6
raksha-dev11 <raksha@drupaltechie.com>
2026-06-23 17:59:03 +0530

    added property images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def property_image_url(property, index)
+    if property.images[index].present?
+      property.images[index]
+    else
+      placeholders = [
+        "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?auto=format&fit=crop&w=1200&q=80",
+        "https://images.unsplash.com/photo-1513694203232-719a280e022f?auto=format&fit=crop&w=800&q=80",
+        "https://images.unsplash.com/photo-1484154218962-a197022b5858?auto=format&fit=crop&w=800&q=80",
+        "https://images.unsplash.com/photo-1507089947368-19c1da9775ae?auto=format&fit=crop&w=800&q=80",
+        "https://images.unsplash.com/photo-1505691938895-1758d7feb511?auto=format&fit=crop&w=800&q=80"
+      ]
+      placeholders[index % placeholders.size]
+    end
+  end
 end
